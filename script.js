0695642177f510a320d945c12b7a68d734ae3ed3
NRHbb <nrhbb14@gmail.com>
2022-12-10 11:32:56 +0800

    update all file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ class player {
     constructor() {
         this.choose = null;
     }
-    getPlayerChoose(choose) {
+    setPlayerChoose(choose) {
         this.choose = choose;
     }
 }
@@ -12,76 +12,99 @@ class computer extends player {
     constructor() {
         super();
     }
-    getComputerChoose(){
-        let chooses = ["batu", "kertas", "gunting"];
-        let select = chooses[Math.floor(Math.random() * chooses.length)]; 
+    setComputerChoose() {
+        const chooses = ["com-batu", "com-kertas", "com-gunting"];
+        const select = chooses[Math.floor(Math.random() * chooses.length)];
         return (this.choose = select);
     }
 }
 
 class game {
-    constructor(player, com){
+    constructor(player, com) {
         this.player = player;
         this.computer = com;
         this.result = null;
 
-        this.playerSection = document.querySelectorAll (".player-side button");
-        this.textBox = document.querySelector ("#result");
+        this.playerSection = document.querySelectorAll(".player-side button");
+        this.textBox = document.getElementById("result");
         this.textResult = document.querySelector(".outcome");
-        this.refresh = document.getElementById("refresh");
+        // this.refresh = document.getElementById("refresh");
     }
-    gameResult(player, com){
-        if (player.choose == com.choose) 
-            return (this.result = "draw");
-        if (player.choose == "batu")
-            return com.choose == "gunting"
-                ? (this.result = "player 1 win")
-                : (this.result = "com win");
-        if (player.choose == "kertas")
-            return com.choose == "batu"
-                ? (this.result = "player 1 win")
-                : (this.result = "com win");
-        if (player.choose == "gunting")
-            return com.choose == "kertas"
-                ? (this.result = "player 1 win")
-                : (this.result = "com win");
+    gameResult(player, com) {
+        if (player.choose == "player-batu" && com.choose == "com-batu") {
+            (this.result = "draw");
+        }
+        if (player.choose == "player-kertas" && com.choose == "com-kertas") {
+            (this.result = "draw");
+        }
+        if (player.choose == "player-gunting" && com.choose == "com-gunting") {
+            (this.result = "draw");
+        }
+        if (player.choose == "player-batu") {
+            if (com.choose == "com-gunting") {
+                (this.result = "player 1 win")
+            }
+            else { (this.result = "com win"); }
+        }
+        if (player.choose == "player-kertas") {
+            if (com.choose == "com-batu") {
+                (this.result = "player 1 win")
+            }
+            else {
+                (this.result = "com win");
+            }
+        }
+        if (player.choose == "player-gunting") {
+            if (com.choose == "com-kertas") {
+                (this.result = "player 1 win")
+            }
+            else { (this.result = "com win"); }
+        }
     }
     setActive(player, com) {
-        const p = document.getElementById(player.choose);
-        const c = document.getElementById("com-" + com.choose);
-        p.classList.add("active");
+        console.log("debug", player.choose)
+        let p
+        if (player.choose === "player-batu") {
+            p = document.getElementById("player-batu")
+        } else if (player.choose === "player-kertas") {
+            p = document.getElementById("player-kertas")
+        } else {
+            p = document.getElementById("player-gunting")
+        }
+        p.classList.add("active")
 
-        setTimeout(() => {
-            c.classList.add("active");
-            this.textBox.classList.add(
-                "rotate",
-                "d-flex",
-                "align-items-center",
-                "justify-content-center"
-            );
-            this.textResult.innerHTML = this.result;
-            this.textResult.classList.replace("vs", "game-result");
-            if (this.result == "draw") {
-                this.textBox.classList.add("game-result-draw");
-            } else {
-                this.textBox.classList.add("game-result-win");
-            }
-        }, 0);
+        let c
+        if (com.choose === "com-batu") {
+            c = document.getElementById("com-batu")
+        } else if (com.choose === "com-kertas") {
+            c = document.getElementById("com-kertas")
+        } else {
+            c = document.getElementById("com-gunting")
+        }
+        c.classList.add("active");
+
+        this.textBox.classList.add(
+            "active"
+        );
+        this.textResult.innerHTML = this.result;
+        this.textResult.classList.replace("outcome", "game-result");
+        if (this.result == "draw") {
+            this.textBox.classList.add("game-result-draw");
+        } else {
+            this.textBox.classList.add("game-result-win");
+        }
     }
 
     setRefresh(player, com) {
         const p = document.getElementById(player.choose);
-        const c = document.getElementById("com-" + com.choose);
+        const c = document.getElementById(com.choose);
         p.classList.remove("active");
         c.classList.remove("active");
         this.textBox.classList.remove(
-            "rotate",
-            "d-flex",
-            "align-items-center",
-            "justify-content-center"
+            "active"
         );
         this.textResult.innerHTML = "vs";
-        this.textResult.classList.replace("game-result", "vs");
+        this.textResult.classList.replace("game-result", "outcome");
         if (this.result == "draw") {
             this.textBox.classList.remove("game-result-draw");
         } else {
@@ -91,46 +114,46 @@ class game {
 }
 
 
-let player1 = new player();
+let human = new player();
 let com = new computer();
-let play = new game(player1, com);
+let play = new game(human, com);
 
-
-let counter = 0;
-
-const playerSide = () => {
-    let playerSelect = play.playerSection;
-    playerSelect.forEach((select) => {
-        select.addEventListener("click", () => {
-            if (counter == 0) {
-                
-                let playerSelect = select.id;
-                player1.getPlayerChoose(playerSelect);
-                
-                let comSelect = com.getComputerChoose();
-                
-                let result = play.gameResult(player1, com);
-                
-                play.setActive(player1, com);
-                counter++;
-                
-                console.log(`Player 1 : ${playerSelect}`);
-                setTimeout(() => {
-                    console.log(`Com : ${comSelect}`);
-                    console.log(`Result : ${result}`);
-                }, 2000);
-            }
-        });
-    });
-};
-playerSide();
 
 
 // * refresh buttons
-const refreshBtn = () => {
-    play.refresh.addEventListener("click", () => {
-        play.setRefresh(player1, com);
-        counter = 0;
-    });
-};
-refreshBtn();
+const refresh = document.getElementById("refresh");
+refresh.addEventListener("click", () => {
+    play.setRefresh(human, com);
+    counter = 0;
+});
+
+const btnbatu = document.getElementById("player-batu")
+btnbatu.addEventListener("click", () => {
+    human.setPlayerChoose("player-batu");
+
+    com.setComputerChoose();
+
+    play.gameResult(human, com);
+
+    play.setActive(human, com);
+})
+const btnkertas = document.getElementById("player-kertas")
+btnkertas.addEventListener("click", () => {
+    human.setPlayerChoose("player-kertas");
+
+    com.setComputerChoose();
+
+    play.gameResult(human, com);
+
+    play.setActive(human, com);
+})
+const btngunting = document.getElementById("player-gunting")
+btngunting.addEventListener("click", () => {
+    human.setPlayerChoose("player-gunting");
+
+    com.setComputerChoose();
+
+    play.gameResult(human, com);
+
+    play.setActive(human, com);
+})
